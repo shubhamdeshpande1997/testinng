4b12ad29fb57936f2d7d0baf7406036f7114c573
shubhamdeshpande1997 <85749185+shubhamdeshpande1997@users.noreply.github.com>
2021-06-15 11:22:27 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "/opt/chromedriver File PATH");
+		//System.setProperty("webdriver.chrome.driver", "/opt/chromedriver File PATH");
+		System.setProperty("webdriver.chrome.driver","driver = webdriver.Chrome(executable_path='/home/chromedriver')"); 
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
