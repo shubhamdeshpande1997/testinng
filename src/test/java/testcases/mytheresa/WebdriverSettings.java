6c11b5fe0fabe1373c0cd942ee95ac2565849a13
shubhamdeshpande1997 <85749185+shubhamdeshpande1997@users.noreply.github.com>
2021-06-16 11:53:20 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -9,16 +9,8 @@ public class WebdriverSettings {
 		//System.setProperty("webdriver.chrome.driver", "/opt/chromedriver File PATH");
 		//System.setProperty("webdriver.chrome.driver","/home/vin/Downloads/chromedriver");
 		System.setProperty("webdriver.chrome.driver","/home/neebal/Downloads/chromedriver"); 
-		WebDriver driver = new ChromeDriver(options);
-		//ChromeOptions options = new ChromeOptions();
-                options.addArguments("start-maximized"); // open Browser in maximized mode
-                options.addArguments("disable-infobars"); // disabling infobars
-                options.addArguments("--disable-extensions"); // disabling extensions
-                options.addArguments("--disable-gpu"); // applicable to windows os only
-                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                options.addArguments("--no-sandbox"); // Bypass OS security model
-                //WebDriver driver = new ChromeDriver(options);
-                //driver.get("https://google.com");
+		WebDriver driver = new ChromeDriver();
+		
 		return driver;
 	}
 
